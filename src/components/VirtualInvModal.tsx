1f1300bb38f35ced56e5563914437a83c21ca004
momen-cn <momen@capitalnumbers.com>
2024-09-10 10:46:52 +0300

    fear[Frontend]:Add Inventory Modal

--- src/components/VirtualInvModal.tsx
+++ src/components/VirtualInvModal.tsx
@@ -1,25 +1,23 @@
 import React from "react";
-import { Modal, Button} from "antd";
+import { Modal, Button } from "antd";
 import VirtualInventory from "../pages/VirtualInventory";
 interface VirtualInvProps {
-        visible: boolean;
-        onClose: () => void;
-      }
-
-const  VirtualInvModal: React.FC<VirtualInvProps> =({visible, onClose})=> {
-  return (
-        <Modal
-        title="Enter Product Code"
-        width={1000}    
-        visible={visible}
-        onCancel={onClose}  
-        footer={[
-
-        ]}
-      >
-        <VirtualInventory />
-      </Modal>
-  )
+  visible: boolean;
+  onClose: () => void;
 }
+
+const VirtualInvModal: React.FC<VirtualInvProps> = ({ visible, onClose }) => {
+  return (
+    <Modal
+      title="Enter Product Code"
+      width={1000}
+      visible={visible}
+      onCancel={onClose}
+      footer={[]}
+    >
+      <VirtualInventory />
+    </Modal>
+  );
+};
 
 export default VirtualInvModal;
