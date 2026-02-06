15cdad5c8391521a51d362a219a1ca1f5f4100f4
momen-cn <momen@capitalnumbers.com>
2026-02-06 12:29:56 +0200

    shopify

--- src/components/ShopifyAuthWaiting.tsx
+++ src/components/ShopifyAuthWaiting.tsx
@@ -92,7 +92,7 @@ const ShopifyAuthWaiting: React.FC<ShopifyAuthWaitingProps> = ({
             }
             
             // Redirect back to Shopify store admin with account_key in URL
-            const shopifyRedirectUrl = `http://indicated-laundry-anti-decor.trycloudflare.com/auth/finerworks-callback?shop=finerworks-dev-3.myshopify.com&account_key=${accountKey}`;
+            const shopifyRedirectUrl = `shopify.finerworks.com/auth/finerworks-callback?shop=finerworks-dev-3.myshopify.com&account_key=${accountKey}`;
             
             console.log('🔄 Redirecting to Shopify store:', shopifyRedirectUrl);
             
